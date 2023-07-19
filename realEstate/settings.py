099c5a460f0b81a3573969578bc15df7f38fdc04
Saish Yeole <100746486+saish17@users.noreply.github.com>
2023-07-19 11:55:58 +0530

    Update settings.py

--- realEstate/settings.py
+++ realEstate/settings.py
@@ -149,7 +149,7 @@ STATICFILES_DIRS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'saishyeole45@gmail.com'
-EMAIL_HOST_PASSWORD = 'ehtahopaarpswatr'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
